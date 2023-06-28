ecb8329d41becafdd562079de32fee2e2c7aeaac
Tibet Erol <tibet.erol@gratas.com.tr>
2023-06-28 19:19:24 +0300

    test 2 (dosya silme,kaydetme,okuma)

--- cards/deck_test.go
+++ cards/deck_test.go
@@ -1,6 +1,9 @@
 package main
 
-import "testing"
+import (
+	"os"
+	"testing"
+)
 
 func TestCreateNewDeck(t *testing.T) {
 	deck := newDeck()
@@ -8,4 +11,18 @@ func TestCreateNewDeck(t *testing.T) {
 	if len(deck) != 16 {
 		t.Errorf("Expected deck length of 16, but got %v", len(deck))
 	}
+}
+
+func TestSaveAndLoadFile(t *testing.T) {
+	os.Remove("_testFile")
+	deck := newDeck()
+	deck.saveToDisk("_testFile")
+
+	loadedDeck := newDeckFromFile("_testFile")
+
+	if len(loadedDeck) != 16 {
+		t.Errorf("Expected deck length of 16, but got %v !", len(deck))
+	}
+
+	os.Remove("_testFile")
 }
