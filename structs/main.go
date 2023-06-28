e40a6bb7d23974b542ecb1ecb7e0157436484904
Tibet Erol <tibet.erol@gratas.com.tr>
2023-06-29 00:00:45 +0300

    update struct

--- structs/main.go
+++ structs/main.go
@@ -9,9 +9,13 @@ type person struct {
 
 func main() {
 
-	//john := person{"John","Wick"} // istenilmeyen kullanim
-	john := person{firstName: "John", lastName: "Wick"} // dogru kullanim ornegi
+	//john := person{firstName: "John", lastName: "Wick"}
+	var aziz person
 
-	fmt.Println(john)
+	aziz.firstName = "Aziz"
+	aziz.lastName = "Vefa"
+	fmt.Printf("%+v", aziz) // %+v ile structin icindeki tum key value lari basar {firstName:Aziz lastName:Vefa}
+	//eger deger atamasaydik bos string olacakti. Diger dillerde genelde null ama burada bos string eger intse 0 ataniyor
+	//fmt.Println(john)
 
 }
