0a978b65b9f9e5a56cc01b022711e4fb24f498e8
Tibet Erol <tibet.erol@gratas.com.tr>
2023-06-30 01:55:17 +0300

    generic deneme , paket indirme denemesi, ici bos struct denemesi
    youtube dan

--- maps/main.go
+++ maps/main.go
@@ -1,25 +1,28 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"golang.org/x/exp/constraints"
+)
 
 type renk map[string]string
 
+type biBakam struct{} // ici bos atruct tanimlanabiliyor istenirse ama tabi lazim bi kullanim degil
+
 func main() {
 
+	adana := biBakam{}
+	adana.denensin()
+	fmt.Println(sum(1.4, 6.8))
 	/*colors := map[string]string{ // map tanimlama alternatif 3 ve ayni zamanda degerler direk ataniyor
 		"red":    "#fff42",
 		"yellow": "#fff423",
 		"white":  "#546546",
 	}*/
 
-	colors2 := renk{
-		"red":    "#fff42",
-		"yellow": "#fff423",
-		"white":  "#546546",
-	}
-
-	//printMap(colors)
-	colors2.printRenk()
+	//v.18 ile birlikte 2022 yılında cıkmıs bu versiyon go da genericlerde kullanilabiliyor
+	// generic icin en guzel yol constraints.Ordered ile kullanmak ve bu bir import gerektiriyor golang.org/x/exp/constraints
+	//fonk.larda,maplerde ve structlarda kullanilabiliyor
 
 }
 
@@ -33,4 +36,12 @@ func (r renk) printRenk() {
 	for key, value := range r {
 		fmt.Println("Hex code for ", key, " is: ", value)
 	}
+}
+
+func sum[T constraints.Ordered](a T, b T) T {
+	return a + b
+}
+
+func (biBakam) denensin() { // burada bi struct receiver olarak verildi ancak receiver olarak b biBakam seklinde instance verilmedi kullanilmayacagindan dolayi. Sayet b biBakam deseydim hata vermezdi ancak kullanilmayacak seyleri tanimlamak best practice degil
+	println("deneme")
 }
