bc1a9eadd08c18df2933342df076d381383c2661
Tibet Erol <tibet.erol@gratas.com.tr>
2023-06-29 18:27:35 +0300

    type olusturup mapi basma
    boylelikle receiver func olusturabildim ve bir map typei olan renk i basabildim

--- maps/main.go
+++ maps/main.go
@@ -2,20 +2,35 @@ package main
 
 import "fmt"
 
+type renk map[string]string
+
 func main() {
 
-	colors := map[string]string{ // map tanimlama alternatif 3 ve ayni zamanda degerler direk ataniyor
+	/*colors := map[string]string{ // map tanimlama alternatif 3 ve ayni zamanda degerler direk ataniyor
+		"red":    "#fff42",
+		"yellow": "#fff423",
+		"white":  "#546546",
+	}*/
+
+	colors2 := renk{
 		"red":    "#fff42",
 		"yellow": "#fff423",
 		"white":  "#546546",
 	}
 
-	printMap(colors)
+	//printMap(colors)
+	colors2.printRenk()
 
 }
 
 func printMap(m map[string]string) {
 	for key, value := range m {
+		fmt.Println("Hex code for ", key, " is: ", value)
+	}
+}
+
+func (r renk) printRenk() {
+	for key, value := range r {
 		fmt.Println("Hex code for ", key, " is: ", value)
 	}
 }
