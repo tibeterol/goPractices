fe825e33fb60f8a1b3a43a29fec8e283a01e5e9d
Tibet Erol <tibet.erol@gratas.com.tr>
2023-06-29 15:51:47 +0300

    map ekleme,silme,guncelleme ve alternatif map tanimlama

--- maps/main.go
+++ maps/main.go
@@ -4,11 +4,27 @@ import "fmt"
 
 func main() {
 
-	colors := map[string]string{
+	//var colors map[string]string // map tanimlama alternatif 1
+
+	var colors = make(map[string]string) // map tanimlama alternatif 2
+	var colors2 = make(map[int]string)
+	/*colors := map[string]string{ // map tanimlama alternatif 3 ve ayni zamanda degerler direk ataniyor
 		"red":    "#fff42",
 		"yellow": "#fff423",
-	}
+	} */
+
+	colors["white"] = "#45456" // mape eleman ekleme ya da guncelleme
+
+	colors["white"] = "#121212"
+
+	colors["red"] = "#78787"
+
+	colors2[10] = "red"
+
+	delete(colors, "red") // mapten eleman silme
+	delete(colors2, 10)
 
 	fmt.Println(colors)
+	fmt.Println(colors2)
 
 }
