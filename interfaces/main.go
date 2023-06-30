f7d496499e9a638d408829b0b27dacae2de087cc
Tibet Erol <tibet.erol@gratas.com.tr>
2023-06-30 07:16:29 +0300

    read fonk.

--- interfaces/main.go
+++ interfaces/main.go
@@ -1,32 +1,21 @@
 package main
 
-import "fmt"
-
-type bot interface {
-	getGreeting() string
-}
-
-type englishBot struct{}
-type spanishBot struct{}
+import (
+	"fmt"
+	"net/http"
+)
 
 func main() {
 
-	e := englishBot{}
-	s := spanishBot{}
+	resp, err := http.Get("http://google.com")
 
-	printGreeting(e)
-	printGreeting(s)
+	if err == nil {
 
-}
+		bs := make([]byte, 99999) // 99.999 yer ayirdik. Cunku read fonk. slicei resize etmiyor. eger 0 lengthli byte slice yollasaydik parametre olarak bu zaten dolu diyip verileri aktarmayacakti
+		resp.Body.Read(bs)
 
-func printGreeting(b bot) {
-	fmt.Println(b.getGreeting())
-}
+		fmt.Println(string(bs)) // byte slice i dogrudan stringe cevirebiliyoruz
 
-func (englishBot) getGreeting() string {
-	return "Hello"
-}
+	}
 
-func (spanishBot) getGreeting() string {
-	return "Hola"
 }
