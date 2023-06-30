52b40e250e15bf5faabde4efbe0e1e29a215a934
Tibet Erol <tibet.erol@gratas.com.tr>
2023-06-30 21:49:37 +0300

    custom write fonk.
    bu fonk. olusturmakla writer interfaceini implement etmis olduk boylece io.Copy i kendi yarattigimiz tiple kullanabildik.

--- interfaces/main.go
+++ interfaces/main.go
@@ -1,19 +1,29 @@
 package main
 
 import (
+	"fmt"
 	"io"
 	"net/http"
-	"os"
 )
+
+type logWriter struct{}
 
 func main() {
 
 	resp, err := http.Get("http://google.com")
+	l := logWriter{}
 
 	if err == nil {
 
-		io.Copy(os.Stdout, resp.Body)
+		io.Copy(l, resp.Body)
 
 	}
 
+}
+
+func (l logWriter) Write(bs []byte) (int, error) {
+
+	fmt.Println(string(bs))
+	fmt.Println("boyut = ", len(bs))
+	return len(bs), nil
 }
