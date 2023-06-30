ccdf207dec0085cdfad9d3e38fee2fd30fa3c9d2
Tibet Erol <tibet.erol@gratas.com.tr>
2023-06-30 23:37:15 +0300

    assignment 2 (kendi cozumum)

--- interfaces/main.go
+++ interfaces/main.go
@@ -1,43 +1,14 @@
 package main
 
-import "fmt"
-
-type shape interface {
-	calculateArea() float64
-}
-
-type triangle struct {
-	height float64
-	base   float64
-}
-
-type square struct {
-	sideLength float64
-}
+import (
+	"io"
+	"os"
+)
 
 func main() {
 
-	t := triangle{
-		height: 7,
-		base:   3,
-	}
-
-	s := square{
-		sideLength: 10,
-	}
-
-	printArea(s)
-	printArea(t)
-}
-
-func (t triangle) calculateArea() float64 {
-	return 0.5 * t.base * t.height
-}
-
-func (s square) calculateArea() float64 {
-	return s.sideLength * s.sideLength
-}
-
-func printArea(sh shape) {
-	fmt.Println(sh.calculateArea())
+	fileName := os.Args[1]
+	file, _ := os.Open(fileName)
+	io.Copy(os.Stdout, file)
+	
 }
