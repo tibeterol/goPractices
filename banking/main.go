680eb3e81dd31022a5def02912bc5bc9a72361cf
Tibet Erol <tibet.erol@gratas.com.tr>
2023-07-01 22:08:52 +0300

    greet fonk. unu ayri olarak yazdik

--- banking/main.go
+++ banking/main.go
@@ -7,11 +7,13 @@ import (
 
 func main() {
 
-	http.HandleFunc("/greet", func(writer http.ResponseWriter, request *http.Request) {
-		fmt.Fprint(writer, "Hello World!")
-
-	})
+	http.HandleFunc("/greet", greet)
 
 	http.ListenAndServe("localhost:8080", nil)
+
+}
+
+func greet(writer http.ResponseWriter, request *http.Request) {
+	fmt.Fprint(writer, "Hello World!")
 
 }
