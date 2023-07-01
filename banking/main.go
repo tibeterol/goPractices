8b77eb81325588b992ac371c95ad44b61efa2e31
Tibet Erol <tibet.erol@gratas.com.tr>
2023-07-01 23:54:49 +0300

    json encoding

--- banking/main.go
+++ banking/main.go
@@ -1,14 +1,22 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
 )
 
+type customer struct {
+	Name    string `json:"full_name"`
+	City    string `json:"city"`
+	ZipCode string `json:"zip_code"`
+}
+
 func main() {
 
 	http.HandleFunc("/greet", greet)
+	http.HandleFunc("/customersList", getCustomers)
 
 	log.Fatal(http.ListenAndServe("localhost:80", nil))
 
@@ -17,4 +25,15 @@ func main() {
 func greet(writer http.ResponseWriter, request *http.Request) {
 	fmt.Fprint(writer, "Hello World!")
 
+}
+
+func getCustomers(w http.ResponseWriter, request *http.Request) {
+	customerList := []customer{
+		{Name: "Tibet", City: "İstanbul", ZipCode: "34718"},
+		{Name: "Betül", City: "İzmir", ZipCode: "35718"},
+	}
+
+	w.Header().Add("Content-Type", "application/json")
+
+	json.NewEncoder(w).Encode(customerList)
 }
